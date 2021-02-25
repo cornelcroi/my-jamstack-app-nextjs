160178382b5d258c7b52dc206642a27aef9b5009
Corneliu Croitoru <cornel.croitoru@gmail.com>
2021-02-25 16:46:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Edge Hackatjon!
+          Welcome to Edge Hackathon!
         </h1>
 
         <p className={styles.description}>
