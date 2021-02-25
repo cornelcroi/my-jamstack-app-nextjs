3e42f759a7bf4c491a27aa9ba8129f6ea50a171b
Corneliu Croitoru <cornel.croitoru@gmail.com>
2021-02-25 08:08:03 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a> from Amplify
+          Welcome to <a href="https://nextjs.org">Next.js!</a> from Amplify 2.0
         </h1>
 
         <p className={styles.description}>
